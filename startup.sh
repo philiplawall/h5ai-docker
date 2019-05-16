2ebbb151fa54aa690b5d546fde0c066dc89de0ce
Richard Challis <rjchallis@gmail.com>
2019-05-16 10:25:27 +0000

    enable custom Masthead

--- startup.sh
+++ startup.sh
@@ -21,7 +21,12 @@ if [ -s /conf/_h5ai.footers.html ]; then
 fi
 
 if [ -s /conf/Masthead.html ]; then
-  sed -i '/<body id="root" class="index">/ r /conf/Masthead.html' /var/www/_h5ai/private/php/pages/index.php
+#  sed -i '/<body[^>]*>/ r /conf/Masthead.html' /var/www/_h5ai/private/php/pages/index.php
+  sed -i 's/<\/head>/<\/head>\n/' /var/www/_h5ai/private/php/pages/index.php
+  sed -i '/<\/head>/ r /conf/Masthead.html' /var/www/_h5ai/private/php/pages/index.php
+  sed -i 's/<body[^>]*>/<outer class="index" id="root"/' /var/www/_h5ai/private/php/pages/index.php
+  sed -i 's/<\/head>/<\/head><body>' /var/www/_h5ai/private/php/pages/index.php
+  sed -i 's/o="body"/o="outer"/g' /var/www/_h5ai/public/js/scripts.js
 fi
 
 if [ -d /conf/img ]; then
